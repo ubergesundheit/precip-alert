4359fa7c76b34e2df8e3d0bc7754043cecda5a07
ubergesundheit <ubergesundheit@googlemail.com>
2014-01-08 15:47:14 +0100

    add coalesce option to ensure same image sizes in exploded gif

--- get_rain.sh
+++ get_rain.sh
@@ -2,4 +2,4 @@
 
 mkdir -p tmp
 wget --quiet -O "tmp/tmp.gif" "http://www.wetteronline.de/?pid=p_radar_map&ireq=true&src=radar/vermarktung/p_radar_map_forecast/forecastLoop/NRW/latestForecastLoop.gif"
-gm convert tmp/tmp.gif +adjoin tmp/regen_%d.png
+gm convert tmp/tmp.gif -coalesce +adjoin tmp/regen_%d.png

2fd7ef3e61cb44ad74f684e03ac8048748fc1f2b
ubergesundheit <ubergesundheit@googlemail.com>
2014-01-10 15:52:13 +0100

    module for time reading, beginning of new comparision method

--- analyze_img.rb
+++ analyze_img.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'oily_png'
-
+require './time_reader'
 
 # hard coded values for regenradar images from wetteronline.de
 
@@ -26,64 +26,7 @@ def hex(pixel)
   ChunkyPNG::Color.to_hex(pixel, false)
 end
 
-def parse_time(image = ChunkyPNG::Image.from_file("tmp/regen_0.png"))
-  white = "#fcfafc"
-  
-  # first digit: check for 0,1,2
-  if hex(image[11,15]) == white # first digit is a 0
-    first = 0
-  elsif hex(image[14,15]) == white # first digit is a 1
-    first = 1
-  else # only digit left: 2
-    first = 2
-  end
 
-  # second digit: check for 0 to 9
-  if hex(image[27,17]) == white # 4
-    second = 4
-  elsif hex(image[26,17]) == white # 6
-    second = 6
-  elsif hex(image[20,15]) == white # 0 or 5
-    if hex(image[26,11]) == white
-      second = 5 
-    else
-      second = 0
-    end
-  elsif hex(image[25,20]) == white # 1 or 2
-    if hex(image[26,20]) == white
-      second = 2
-    else
-      second = 1
-    end
-  elsif hex(image[22,15]) == white # 3 or 8
-    if hex(image[20,13]) == white
-      second = 8
-    else
-      second = 3
-    end
-  elsif hex(image[20,11]) == white # 7
-    second = 7
-  else
-    second = 9
-  end
-
-  # third digit: check for 0,1,3,4
-  # is it a 4?
-  if hex(image[42,17]) == white
-    minutes = 45
-  elsif hex(image[38,15]) == white # either a 1 or 3
-    if hex(image[41,13]) == white # its a 3
-      minutes = 30
-    else
-      minutes = 15
-    end
-  else # its a 0
-    minutes = 0
-  end
-
-  now = Time.now
-  Time.new(now.year, now.month, now.day, "#{first}#{second}".to_i, minutes)
-end 
 
 # execute shell script to download
 # and split the rain gif
@@ -91,7 +34,8 @@ end
 
 
 def parse_imgs
-  time_first_img = parse_time
+  time_reader = TimeReader.new
+  time_first_img = time_reader.parse_time
   result = {}
 
   # discard first & second image and look at the rest of the images
@@ -99,7 +43,6 @@ def parse_imgs
     image = ChunkyPNG::Image.from_file("tmp/regen_#{i}.png")
  
     current_img_result = []
-
     @pixels_to_check.each do |coords|
       pixel_color = hex(image[coords[0],coords[1]])
       if @rain_strengths.values.include?(pixel_color)
@@ -129,4 +72,7 @@ def make_human_readable(times)
   output
 end
 
-puts make_human_readable(parse_imgs)
+t= TimeReader.new
+t.read_time
+
+# puts make_human_readable(parse_imgs)
---
+++ time_reader.rb
@@ -0,0 +1,106 @@
+class TimeReader
+#first digit position (11,11,8,10)
+#second digit position (20,11,8,10)
+#third digit position (35,11,8,10)
+  
+
+  def initialize(image = ChunkyPNG::Image.from_file("tmp/regen_0.png"))
+    @image = image
+@digits_data =["data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAKBAMAAAB76QKzAAAAKlBMVEUMCgwUFhQcHiQsKiw0MjRERkRUVlRkZmR8enyEhoS8urzc2tzs6uz8+vzNe8hcAAAAPUlEQVR4nAEyAM3/Ajm9yTMCQqeSQAIsz8wwAh3gDSACDxDfEAL+7v7+AvIAA/AC4wAC4ALFQEXAAq1sXa/YvhOyMRR+IwAAAABJRU5ErkJggg==",
+  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAKBAMAAAB76QKzAAAAHlBMVEUMCgwcGhwcHiQcIhwcIiQkHhwsKiw0LjRcWlz8+vwtaPfIAAAANklEQVR4nGNK7yxLY3ok8IGBSfbCgQ9MvxmsuZk2bWDdwPQv4DcDE8OGDUACCEDEfyaOhg4GAMKnD3DMknedAAAAAElFTkSuQmCC",
+  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAKBAMAAAB76QKzAAAALVBMVEUMCgwcGhwkHhwsKiw0MjRERkRcWlx8enyMjoycnpy8urzEwsTc2tzk5uT8+vxTLd1gAAAAOklEQVR4nGPqenfTmck2NC+BKefBGzume3xMj5gYGMQmMCmwZ+1jejIhhoHJYyYDA1PEBiBheffueQC3rxC/f7be9QAAAABJRU5ErkJggg==",
+  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAKCAMAAAC+Ge+yAAAAOVBMVEUMCgwUFhQcHiQcIhwkHhwsKiw0MjQ8QjxERkRkZmR8enyEhoSUlpSsrqy8urzExsTc2tzs6uz8+vw7xiP/AAAAUUlEQVR4nB3KQQ6AIAwEwGVFIERM5P9/9KCpIiXVOOdhLlutJQTOQ47TslG6wh4aocSUbhC+lKyX49D9EP8dA7RdcRA/A9fkEXMz9nnZUhf3AhbhIWsggmGBAAAAAElFTkSuQmCC",
+  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAKBAMAAAB76QKzAAAAJFBMVEUMCgwUFhQkHhwsKixEPjxERkRUVlRkZmR8enyEhoTs6uz8+vxwPYQOAAAAOklEQVR4nGMyNp5tzMTAKsDA9MHpAAOTkM6Ff0yipxkeMMUdeMDEJNy1YAVTWFhCGBPDfwYGJiD+DwBlAQ9gUBVoiQAAAABJRU5ErkJggg==",
+  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAKBAMAAAB76QKzAAAAJFBMVEUMCgwcGhwsKiw0MjRMTkxcWlxsamx8eny8urzExsTk5uT8+vwxF2N4AAAAO0lEQVR4nGPavXv3ZqbvGen5TAz//zMw/f/P+J+Ja1bpR6aw8I3xQLFTBkwMDAwfmFoEzN8zPZ7lOwEAM/QU7pMnr5gAAAAASUVORK5CYII=",
+  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAKBAMAAAB76QKzAAAAMFBMVEUMCgwcGhwcHiQkHhwsKiw0MjRMSkxcWlxkZmR8enysqqy8urzEwsTc2tzk5uT8+vwXrgK9AAAAPUlEQVR4nAEyAM3/AkSN/0QCBylFAAJQjgDeAiz9/SACM66lAQL+ZXMQAum9u54C1gAAzwLGUEaBApt+bM+2jxLiAQpLEQAAAABJRU5ErkJggg==",
+  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAKBAMAAAB76QKzAAAALVBMVEUMCgwcGhwcHiQkHhwsKiw0MjRMTkxsamx8enyEhoSUlpSkoqTExsTc3tz8+vySNro6AAAAO0lEQVR4nGN69+7dU6aw8Mj7TA/+sC1gkr9sd5fpnIEXAxPD1XsXmBhM+oCsmAsMTMyaDAxM39a//w8AIz4TsEjwZwcAAAAASUVORK5CYII=",
+  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAKCAMAAAC+Ge+yAAAAM1BMVEUMCgwcGhwkHhwsKiw0MjRMTkxcWlxkZmR8enyEhoSUkpSkpqS8urzMyszc3tzk5uT8+vz6kOH7AAAAUElEQVR4nA3KWxJAMBAEwMlkCbUK979mKPLa5bubosd5qgiXcefLVqd0h7fo7FMwzMP5xG3TqfCn/L4tGpdivdUEBgIwgDVFYK5/hh57ePABVFwiWBL90ywAAAAASUVORK5CYII=",
+  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAKBAMAAAB76QKzAAAALVBMVEUMCgwcGhwkHhwsKiw0MjRMSkxUVlRsamx8enysqqy8urzExsTc2tzk5uT8+vyzPfHZAAAAPUlEQVR4nAEyAM3/AkfOyDMCI4WUQAJK4No+AjD+3iICdBIzEAK+m5L/AvszPM8CAAAFwAIAA4CgAg3X5//S7RKv1chbPwAAAABJRU5ErkJggg=="]
+  end
+
+  def read_time
+    image = ChunkyPNG::Image.from_file("tmp/regen_4.png")
+    first = image.crop(11,11,8,10)
+    #second = image.crop(20,11,8,10)
+    #third = image.crop(35,11,8,10)
+
+    digits = []
+    @digits_data.each do |data|
+      digits << ChunkyPNG::Image.from_data_url(data)
+    end
+
+    
+
+  end
+
+def string_difference_percent(a, b)
+  longer = [a.size, b.size].max
+  same = a.each_char.zip(b.each_char).select { |a,b| a == b }.size
+  (longer - same) / a.size.to_f
+end
+
+  def parse_time
+    white = "#fcfafc"
+    
+    # first digit: check for 0,1,2
+    if hex(@image[11,15]) == white # first digit is a 0
+      first = 0
+    elsif hex(@image[14,15]) == white # first digit is a 1
+      first = 1
+    else # only digit left: 2
+      first = 2
+    end
+  
+    # second digit: check for 0 to 9
+    if hex(@image[27,17]) == white # 4
+      second = 4
+    elsif hex(@image[26,17]) == white # 6
+      second = 6
+    elsif hex(@image[20,15]) == white # 0 or 5
+      if hex(@image[26,11]) == white
+        second = 5 
+      else
+        second = 0
+      end
+    elsif hex(@image[25,20]) == white # 1 or 2
+      if hex(@image[26,20]) == white
+        second = 2
+      else
+        second = 1
+      end
+    elsif hex(@image[22,15]) == white # 3 or 8
+      if hex(@image[20,13]) == white
+        second = 8
+      else
+        second = 3
+      end
+    elsif hex(@image[20,11]) == white # 7
+      second = 7
+    else
+      second = 9
+    end
+  
+    # third digit: check for 0,1,3,4
+    # is it a 4?
+    if hex(@image[42,17]) == white
+      minutes = 45
+    elsif hex(@image[38,15]) == white # either a 1 or 3
+      if hex(@image[41,13]) == white # its a 3
+        minutes = 30
+      else
+        minutes = 15
+      end
+    else # its a 0
+      minutes = 0
+    end
+  
+    now = Time.now
+    Time.new(now.year, now.month, now.day, "#{first}#{second}".to_i, minutes)
+  end 
+
+  private
+
+    def compare_digit(digit_image)
+     #image.crop(10,11,8,10).save('wat.png')  
+    end
+end
